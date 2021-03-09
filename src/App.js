5cfae3e0cea850c02596a8341d73928c55fb234f
Git Bash <bmarkoski@gmail.com>
2021-03-09 14:09:03 +0100

    broken our homapage into smaller components, finished styling our meny-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./HomePage";
+import HomePage from "./pages/homapage/HomePage";
 
 function App() {
   return (
---
+++ src/components/menu-item/MenyItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./menu-item.styles.scss";
+
+const MenyItem = ({ title, imageURL, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        style={{
+          backgroundImage: `url(${imageURL})`,
+        }}
+        className="background-image"
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenyItem;
