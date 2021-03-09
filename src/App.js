34ad684f560c10a8ce8d57bf80525199ca39245c
Git Bash <bmarkoski@gmail.com>
2021-03-09 18:34:18 +0100

    adding header component and links to home, shop page and contanct

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+import Header from "./components/header/Header";
 import Hats from "./pages/hats/Hats";
 import HomePage from "./pages/homapage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
@@ -8,6 +9,7 @@ import ShopPage from "./pages/shop/ShopPage";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
